05f122f6d0e5d9b16660a46aa615ff784d5f356b
Max Weiner <mawa30@lehigh.edu>
2026-09-09 15:22:26 -0400

    Added program 3.py

---
+++ Programming Activities/program3.py
@@ -0,0 +1,4 @@
+# Revision of Program 2 on September 9
+#Print out our name 
+message = "My Name is Max Weiner!" # save "My Name is Max Weiner!" to the variable message
+print(message) # output the saved message to the REPL
